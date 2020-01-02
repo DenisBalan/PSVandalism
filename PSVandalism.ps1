69418e7e5caefa60a846131b4671fdf0afc4c061
Denis Balan <33955091+DenisBalan@users.noreply.github.com>
2020-01-02 17:05:27 +0200

    Update PSVandalism.ps1

--- PSVandalism.ps1
+++ PSVandalism.ps1
@@ -4,7 +4,8 @@ iex $($(iwr $("https://pokrovsky.herokuapp.com/$(`
     read-host 'text')))"))`
  |% content `
  |% {$_ -replace @('#!/bin/bash', '#Requires -Version 3.0') } `
- |% {$_ -replace @('touch', 'ni') } |% {$_ -replace @('GIT_', '$env:GIT_') } `
+ |% {$_ -replace @('touch', 'ni') } `
+ |% {$_ -replace @('GIT_', '$env:GIT_') } `
  |% {$_ -replace @('/dev/null', '$null') } `
  |% {$_ -replace @('DATE=', 'DATE="') } `
  |% {$_ -replace @('00:00', '00:00"; ') })
